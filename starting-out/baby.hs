01d9b1e5cbdae5cd975acfff274f2772f3e21a3f
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-17 16:18:51 -0800

    Creates doubleSemallNumber plus 1

--- starting-out/baby.hs
+++ starting-out/baby.hs
@@ -5,3 +5,5 @@ doubleUs :: (Num a) => a -> a -> a
 doubleUs x y = doubleMe x + doubleMe y
 
 doubleSmallNumber x = if x > 100 then x else x * 2
+
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
