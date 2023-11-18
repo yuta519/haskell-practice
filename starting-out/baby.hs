199cfcfaffcd6a57c33132d5f3b0d6601c415401
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-17 16:33:10 -0800

    Adds list completion

--- starting-out/baby.hs
+++ starting-out/baby.hs
@@ -7,3 +7,5 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100 then x else x * 2
 
 doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
+
+listCompletion = [x * 2 | x <- [1 .. 10]]
