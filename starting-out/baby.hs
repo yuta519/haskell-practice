634c5b690c73aea3e715963956f4e3bd636cfeee
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-19 14:29:44 -0800

    Adds another list complition case

--- starting-out/baby.hs
+++ starting-out/baby.hs
@@ -9,3 +9,5 @@ doubleSmallNumber x = if x > 100 then x else x * 2
 doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
 
 listCompletion = [x * 2 | x <- [1 .. 10]]
+
+excludeListComplition = [x | x <- [10..20], x /= 13, x /= 15, x /= 19]
