005a0222b84e3196e7257ed5933fdbaa93f24019
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-26 13:29:07 -0800

    Create tell function
    Tell function is pattern mattching for array

--- 03.syntax-in-functions/index.hs
+++ 03.syntax-in-functions/index.hs
@@ -9,3 +9,12 @@ addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 head' :: [a] -> a
 head' [] = error "Can't call head' on an empty list, dummy!"
 head' (x : _) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+-- tell (x : []) = "The list has one element: " ++ show x
+-- tell (x : y : []) = "The list has two elements: " ++ show x ++ " and " ++ show y
+-- When apply list literal to the above code
+tell [x] = "The list has one element: " ++ show x
+tell [x, y] = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x : y : _) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
