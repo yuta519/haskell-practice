24ced8b8bd82bf20587bff28147c0488dd514c10
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2023-11-27 13:26:26 -0800

    Create myCompare

--- 03.syntax-in-functions/index.hs
+++ 03.syntax-in-functions/index.hs
@@ -35,3 +35,9 @@ max' :: (Ord a) => a -> a -> a
 max' a b
   | a > b = a
   | otherwise = b
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+  | a > b = GT
+  | a == b = EQ
+  | otherwise = LT
